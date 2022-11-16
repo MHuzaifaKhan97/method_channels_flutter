98567f01f67d60c9780de44b15b616745fcafc97
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2022-11-16 15:05:34 +0500

    Method Channels for IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,35 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+          let flutterChannel = FlutterMethodChannel(name: "test.flutter",
+                                                         binaryMessenger: controller.binaryMessenger)
+          flutterChannel.setMethodCallHandler({
+             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+              
+              if(call.method == "getMessage"){
+//                   Return from success
+                  result ("Received callback from iOS")
+                  
+//                  If i want to through Error
+//                  result(FlutterError(code: "-1",
+//                                         message: "something went wrong",
+//                                         details: "something went wrong"))
+              }
+              
+//              Invoke Flutter Method from native side
+              DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                       
+                       DispatchQueue.main.async {
+                         // Call the desired channel message here.
+                       }
+                       
+                       flutterChannel.invokeMethod("fromNative", arguments: "{\"message\":\"Hello from native\"}")
+                   }
+                   
+               })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,20 +12,60 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String message = "";
+  String nativeToFlutter = "";
+
+  static const platform = MethodChannel('test.flutter');
+
+  @override
+  initState() {
+    super.initState();
+    initNativeMethodHandler();
+  }
+
+  Future<void> initNativeMethodHandler() async {
+    platform.setMethodCallHandler((call) async {
+      final argument = call.arguments.toString();
+      switch (call.method) {
+        case "fromNative":
+          Map<String, dynamic> json =
+              jsonDecode(argument) as Map<String, dynamic>;
+          nativeToFlutter = json["message"];
+          break;
+      }
+      setState(() {});
+    });
+  }
+
+  Future<void> getMessageFromNative() async {
+    try {
+      message = await platform.invokeMethod("getMessage", {"param1": 1});
+    } on PlatformException catch (e) {
+      message = "failed to call getMessage $e";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Method Channels Example"),
+      ),
       body: Container(
+          width: MediaQuery.of(context).size.width,
           padding: const EdgeInsets.all(20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ElevatedButton(
-                  onPressed: () async {},
+                  onPressed: () async {
+                    await getMessageFromNative();
+                    setState(() {});
+                  },
                   child: const Text("Get Message from native")),
               const SizedBox(height: 30),
               Text(message),
               const SizedBox(height: 30),
+              Text(nativeToFlutter),
             ],
           )),
     );
